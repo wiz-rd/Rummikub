a2e7c5027e88779e337f6a892f8edb8de4de5347
wiz-rd <126345400+wiz-rd@users.noreply.github.com>
2025-02-21 12:41:34 -0500

    Figuring out HTTPRequests still
    Working my way through Godot HTTPRequests to figure out how they work. Right now, I'm trying to ensure the cookies are stored and sent to the server correctly.

--- godot/rummikub/scripts/create_session.gd
+++ godot/rummikub/scripts/create_session.gd
@@ -3,7 +3,7 @@ extends Control
 
 func _on_login_button_pressed() -> void:
 	var username = $LoginContents/ButtonAndField/UsernameField.text
-	if len(username) >= 1:
+	if len(username) >= 2:
 		SessionHandler.create_username(username)
 	else:
 		# CRITICAL TODO: notify user of this
--- godot/rummikub/scripts/session_handler.gd
+++ godot/rummikub/scripts/session_handler.gd
@@ -1,7 +1,7 @@
 extends Node
 
 var username: String
-var server_url: String = "http://localhost"
+var server_url: String = "http://127.0.0.1"
 var port: int = 8000
 var api_string: String = "/api"
 # TODO:
@@ -10,27 +10,31 @@ var api_string: String = "/api"
 var api_url: String = server_url + ":" + str(port) + api_string
 
 
-# TODO CRITICAL: convert CookieHTTPRequest to a node somehow
-
-
-func create_username(username: String):
+func create_username(usrnm: String):
 	"""
 	Creates a username on the server.
 	"""
 	# using a CookieHTTPRequest instead of a
 	# standard HTTPRequest SHOULD store the
 	# cookies automatically
-	var req := CookieHTTPRequest.new()
-	get_node(".").add_child(req)
+	var req = CookieHTTPRequest.new()
+	add_child(req)
+
+	# TODO: delete debugging
+	var full_url = api_url + "/user/" + usrnm
+	print(full_url)
+
 	req.request_completed.connect(_on_create_username)
-	req.cookie_request(
+	var error = req.cookie_request(
 		# the username is set based on the URL
-		api_url + "/user/" + username,
+		full_url,
 		# no special headers are needed
-		[""],
+		[],
 		# put the username
 		HTTPClient.Method.METHOD_PUT
 	)
+	if error != OK:
+		push_error("Error in setting username.")
 
 
 func get_username():
@@ -38,39 +42,49 @@ func get_username():
 	Updates the username value to what the server has
 	stored as their username.
 	"""
-	var req := CookieHTTPRequest.new()
-	get_node(".").add_child(req)
+	var req = CookieHTTPRequest.new()
+	add_child(req)
+
+	# TODO: delete debugging
+	var full_url = api_url + "/user"
+	print(full_url)
+
 	req.request_completed.connect(_on_get_username)
-	req.cookie_request(
-		# the username is set based on the URL
-		api_url + "/user",
-		# no special headers are needed
-		[""],
-		# put the username
-		HTTPClient.Method.METHOD_GET
-	)
-	
+	var error = req.cookie_request(full_url)
+		## the username is set based on the URL
+		#full_url,
+		## no special headers are needed
+		#[""],
+		## get the username
+		#HTTPClient.Method.METHOD_GET
+	#)
+	if error != OK:
+		push_error("Error in getting username.")
 
 
 func _on_create_username(_result, response_code, _headers, body):
 	"""
 	Attempts to create a username for the given user.
 	"""
-	print(body)
 	print(response_code)
-	if response_code != HTTPClient.RESPONSE_ACCEPTED:
+	if response_code != HTTPClient.RESPONSE_OK:
 		# CRITICAL TODO: Notify the user in some way
 		print("The username creation failed. Reasoning:")
-		print(str(body))
+		print(body.get_string_from_utf8())
 
 
 func _on_get_username(_result, response_code, _headers, body):
 	"""
 	Performs the updating of the username value.
 	"""
-	print("get attempted")
-	if response_code == HTTPClient.ResponseCode.RESPONSE_ACCEPTED:
-		var json = JSON.parse_string(body.get_string_from_utf8())
+	print(_headers)
+	print("body: " + body.get_string_from_utf8())
+	if response_code != HTTPClient.RESPONSE_OK:
+		push_error("There was an error obtaining the username.")
+		# CRITICAL TODO: Notify the user if this fails
+	else:
+		var json = JSON.new()
+		json.parse(body.get_string_from_utf8())
+		var response = json.get_data()
 		# update the global username value
-		username = json["username"]
-	# CRITICAL TODO: Notify the user if this fails
+		username = response["username"]
